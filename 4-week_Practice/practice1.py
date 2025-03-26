546a7c829c216037c9a8a8aa189d83b23e3fc58b
Kimmin_01 <kimmin12327@gmail.com>
2025-03-26 14:10:52 +0900

    feat:조건문 배우기

---
+++ 4-week_Practice/practice1.py
@@ -0,0 +1,13 @@
+import datetime #날짜/시간 기능 가져오기기
+
+now = datetime.datetime.now()
+month = now.month
+
+if 3 <= month <= 5:
+    print("현재는 봄입니다.")
+elif 6 <= month <= 8:
+    print("현재는 여름입니다.")
+elif 9 <= month <= 11:
+    print("현재는 가을을입니다.")
+else:
+    print("현재는 겨울입니다.")
---
+++ 4-week_Practice/practice2.py
@@ -0,0 +1,26 @@
+x = int(input("연도를 입력하시오:"))
+
+if x % 12 == 1:
+    print("쥐띠입니다.")
+elif x % 12 == 2:
+    print("소띠입니다.")
+elif x % 12 == 3:
+    print("호랑이띠입니다.")
+elif x % 12 == 4:
+    print("토끼띠입니다.")
+elif x % 12 == 5:
+    print("용띠입니다.")
+elif x % 12 == 6:
+    print("뱀띠입니다.")
+elif x % 12 == 7:
+    print("말띠입니다.")
+elif x % 12 == 8:
+    print("양띠입니다.")
+elif x % 12 == 9:
+    print("원숭이띠입니다.")
+elif x % 12 == 10:
+    print("닭띠입니다.")
+elif x % 12 == 11:
+    print("개띠입니다.")
+else:
+    print("돼지띠입니다.")
