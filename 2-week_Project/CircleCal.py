921fc71b63c4326822139737c8f8460120421c8a
Kimmin_01 <kimmin12327@gmail.com>
2025-04-23 02:04:16 +0900

    주석 슴->승으로 고침

--- 2-week_Project/CircleCal.py
+++ 2-week_Project/CircleCal.py
@@ -4,7 +4,7 @@ pi = 3.14
 print("둘레:", 2 * pi * radius)
 print("넓이:f", pi * radius ** 2)
 
-import math # math module import하는 다른 방법/코드 길어짐, 정확도 상슴
+import math # math module import하는 다른 방법/코드 길어짐, 정확도 상승
 r = 'radius'
 c = 'circum'
 a = 'area'
